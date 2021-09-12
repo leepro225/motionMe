9512600e2aa475900c7dcc5e953bb679dc202e1c
lee pro <leepro225@gmail.com>
2021-09-12 23:52:12 +0900

    함수 추가

--- build/index.js
+++ build/index.js
@@ -1,3 +1,65 @@
 "use strict";
-console.log('tes222444422t');
+var modalType = 'imageBtn';
+var clickAddBtn = function (id) {
+    var modal = document.getElementById('modal');
+    var viewType = document.getElementById('viewType');
+    modalType = id;
+    viewType === null || viewType === void 0 ? void 0 : viewType.classList.remove('none');
+    modal === null || modal === void 0 ? void 0 : modal.classList.remove('none');
+};
+/**
+ * 팝업을 초기화 하고, 닫는다.
+ */
+var clickCloseBtn = function () {
+    var modal = document.getElementById('modal');
+    var viewType = document.getElementById('viewType');
+    var viewTypeInput = viewType === null || viewType === void 0 ? void 0 : viewType.getElementsByTagName('input');
+    Array.prototype.forEach.call(viewTypeInput, function (elem) {
+        elem.value = '';
+    });
+    viewType === null || viewType === void 0 ? void 0 : viewType.classList.add('none');
+    modal === null || modal === void 0 ? void 0 : modal.classList.add('none');
+};
+var clickSaveBtn = function () {
+    var html;
+    var body = document.getElementById('body');
+    var title = document.getElementById('title');
+    var contents = document.getElementById('contents');
+    switch (modalType) {
+        case 'imageBtn':
+            html = imageContainer(title.value, contents.value);
+            break;
+        case 'videoBtn':
+            html = videoContainer(title.value, contents.value);
+            break;
+        case 'noteBtn':
+            html = noteContainer(title.value, contents.value);
+            break;
+        case 'taskBtn':
+            html = todoContainer(title.value, contents.value);
+            break;
+        default:
+            break;
+    }
+    body === null || body === void 0 ? void 0 : body.appendChild(html);
+    clickCloseBtn();
+};
+var clickRemoveBtn = function (e) {
+};
+var imageContainer = function (title, contents) {
+    var html = "<div class=\"contents-container\">\n                <div class=\"contents\"><img src=\"" + contents + "\"></div>\n                <div class=\"title\">" + title + "</div>\n                <div class=\"close\"><a onclick=\"clickRemoveBtn(e)\">X</a></div>\n            </div>";
+    return html;
+};
+var videoContainer = function (title, contents) {
+    var html = "<div class=\"contents-container\">\n               <div class=\"contents\">\n                    <iframe width=\"100%\" height=\"100%\" src=\"" + contents + "\" title=\"YouTube video player\"\n                        frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\"\n                        allowfullscreen></iframe>\n                 </div>\n                <div class=\"title\">" + title + "</div>\n                <div class=\"close\"><a onclick=\"clickRemoveBtn(e)\">X</a></div>\n            </div>";
+    return html;
+};
+var noteContainer = function (title, contents) {
+    var html = "<div class=\"contents-container\">\n                <div class=\"title\">" + title + "</div>\n                <div class=\"body\">" + contents + "</div>\n                <div class=\"close\"><a onclick=\"clickRemoveBtn(e)\">X</a></div>\n            </div>";
+    return html;
+};
+var todoContainer = function (title, contents) {
+    var html = "<div class=\"contents-container\">\n                <div class=\"title\">" + title + "</div>\n                <div class=\"body\"><input type=\"checkbox\"><label>" + contents + "</label></div>\n                <div class=\"close\"><a onclick=\"clickRemoveBtn(e)\">X</a></div>\n            </div>";
+    return html;
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,101 @@
-console.log('tes222444422t')
+
+type Button = 'imageBtn' | 'videoBtn' | 'noteBtn' | 'taskBtn';
+
+let modalType:Button = 'imageBtn';
+
+const clickAddBtn = (id: Button) => {
+    const modal = document.getElementById('modal');
+    const viewType = document.getElementById('viewType'); 
+    modalType = id;
+
+    viewType?.classList.remove('none');
+    modal?.classList.remove('none');
+}
+
+/**
+ * 팝업을 초기화 하고, 닫는다.
+ */
+const clickCloseBtn = () => {
+    const modal = document.getElementById('modal');
+    const viewType = document.getElementById('viewType');
+    const viewTypeInput = viewType?.getElementsByTagName('input');  
+
+    Array.prototype.forEach.call(viewTypeInput, (elem) => {
+        elem.value = '';
+    });
+
+    viewType?.classList.add('none');
+    modal?.classList.add('none');
+}
+
+const clickSaveBtn = () => {
+    let html;
+    const body = document.getElementById('body');
+
+    const title = <HTMLInputElement>document.getElementById('title');
+    const contents =<HTMLInputElement> document.getElementById('contents');
+
+    switch(modalType) {
+        case 'imageBtn' : html = imageContainer(title.value, contents.value);
+        break;
+        case 'videoBtn' : html = videoContainer(title.value, contents.value);
+        break;
+        case 'noteBtn' : html =  noteContainer(title.value, contents.value);
+        break;
+        case 'taskBtn' : html = todoContainer(title.value, contents.value);
+        break;
+        default:
+        break;
+    }
+
+    body?.appendChild(html);
+    clickCloseBtn();
+}
+
+const clickRemoveBtn = (e: Event) => {
+
+}
+
+const imageContainer = (title: string, contents: string): string => {
+   
+    const html = `<div class="contents-container">
+                <div class="contents"><img src="${contents}"></div>
+                <div class="title">${title}</div>
+                <div class="close"><a onclick="clickRemoveBtn(e)">X</a></div>
+            </div>`
+    return html
+}
+
+const videoContainer = (title: string, contents: string) => {
+    
+    const html = `<div class="contents-container">
+               <div class="contents">
+                    <iframe width="100%" height="100%" src="${contents}" title="YouTube video player"
+                        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                 </div>
+                <div class="title">${title}</div>
+                <div class="close"><a onclick="clickRemoveBtn(e)">X</a></div>
+            </div>`
+    return html
+}
+
+const noteContainer = (title: string, contents: string) => {
+    
+    const html = `<div class="contents-container">
+                <div class="title">${title}</div>
+                <div class="body">${contents}</div>
+                <div class="close"><a onclick="clickRemoveBtn(e)">X</a></div>
+            </div>`
+    return html
+}
+
+const todoContainer = (title: string, contents: string) => {
+    
+    const html = `<div class="contents-container">
+                <div class="title">${title}</div>
+                <div class="body"><input type="checkbox"><label>${contents}</label></div>
+                <div class="close"><a onclick="clickRemoveBtn(e)">X</a></div>
+            </div>`
+    return html
+}
